851af9ff9f2a9ceb8cc16ac4aecee5e108bc3cab
Thorsten von Eicken <tve@voneicken.com>
2022-03-14 13:05:52 -0700

    fix

--- update-package-json.js
+++
@@ -1,5 +0,0 @@
-const fs = rquire('fs')
-let pkg = fs.readFileSync('package.json', 'utf8')
-let nodes = fs.readFileSync('package-nodes.json', 'utf8')
-pkg["node-red"].nodes = nodes
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
