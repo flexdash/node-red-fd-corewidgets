e5488f1bcf52460e2e92bbfc125f839deeb2b100
Thorsten von Eicken <tve@voneicken.com>
2022-04-15 22:59:36 -0700

    fix-up commit mess

--- scripts/generate-nodes.sh
+++ scripts/generate-nodes.sh
@@ -1,8 +1,9 @@
 #! /bin/bash -ex
-FLEXDASH_VERSION=0.3.16
+FLEXDASH_VERSION=0.4.5
 
-npm install
+npm install --no-progress --no-audit --no-fund
 # get flexdash source since the widget sources are there
+rm -rf flexdash-src
 mkdir -p flexdash-src
 curl -L https://s3.amazonaws.com/s3.voneicken.com/flexdash/flexdash-${FLEXDASH_VERSION}-src.tgz | \
     tar -C flexdash-src -zxf - src/widgets
@@ -12,6 +13,6 @@ cp -r flexdash-src/src/widgets widgets
 mkdir widgets/node_modules
 cp -r ./node_modules/uplot widgets/node_modules
 # generate nodes
-node ./node_modules/.bin/gen-widgets
+node ./node_modules/.bin/gen-widget-nodes
 # insert the generated nodes into package.json
 node ./scripts/update-package-json.js
