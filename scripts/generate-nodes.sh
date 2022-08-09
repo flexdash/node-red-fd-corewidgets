dd44837513fc2bb675265e5f4a9d38e99a44bdf3
Thorsten von Eicken <tve@voneicken.com>
2022-08-09 11:47:39 -0700

    generate.sh: use local version of FlexDash sources, if available

--- scripts/generate-nodes.sh
+++ scripts/generate-nodes.sh
@@ -1,14 +1,19 @@
-#! /bin/bash -ex
+#! /bin/bash -e
 FLEXDASH_VERSION=0.4
 
 npm install --no-progress --no-audit --no-fund
 # get flexdash source since the widget sources are there
-rm -rf flexdash-src
-mkdir -p flexdash-src
-curl -L https://s3.amazonaws.com/s3.voneicken.com/flexdash/flexdash-${FLEXDASH_VERSION}-src.tgz | \
+rm -rf flexdash-src widgets
+if [[ -d ../flexdash/src/widgets ]]; then
+  echo "Using FlexDash sources in ../flexdash/src"
+  cp -r ../flexdash/src/widgets .
+else
+  mkdir -p flexdash-src
+  echo Downloading FlexDash ${FLEXDASH_VERSION}
+  curl -L https://s3.amazonaws.com/s3.voneicken.com/flexdash/flexdash-${FLEXDASH_VERSION}-src.tgz | \
     tar -C flexdash-src -zxf - src/widgets
-rm -rf widgets
-cp -r flexdash-src/src/widgets widgets
+  cp -r flexdash-src/src/widgets widgets
+fi
 ( cd widgets; rm *-widget.vue panel.vue random-*.vue thermostat.vue upload-button.vue )
 # hack for uplot dependency
 mkdir widgets/node_modules
@@ -19,6 +24,7 @@ if [[ -f ../node-red-flexdash/gen-widget-nodes.js ]]; then
     echo Using source gen-widget-nodes
     node ../node-red-flexdash/gen-widget-nodes.js
 else
+    echo Using NPM gen-widget-nodes
     node ./node_modules/.bin/gen-widget-nodes
 fi
 # insert the generated nodes into package.json
