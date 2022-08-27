7f28bacf1ade7ca067d6f3056ce4149fc0028aa6
Thorsten von Eicken <tve@voneicken.com>
2022-08-27 08:45:59 -0700

    fix generation of package.json: need EOL at end of file

--- scripts/update-package-json.js
+++ scripts/update-package-json.js
@@ -2,4 +2,5 @@ const fs = require('fs')
 let pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 let nodes = JSON.parse(fs.readFileSync('package-nodes.json', 'utf8'))
 pkg["node-red"].nodes = nodes
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2))
+const json = JSON.stringify(pkg, null, 2) + '\n'
+fs.writeFileSync('package.json', json)
